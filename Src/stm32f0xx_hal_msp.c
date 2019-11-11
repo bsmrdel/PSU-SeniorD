c9b240ce6adc80b83477e000131fda6ba5b3615b
Brad Smrdel <bsmrdel@purdue.edu>
2019-11-11 17:00:57 -0500

    working buck

--- Src/stm32f0xx_hal_msp.c
+++ Src/stm32f0xx_hal_msp.c
@@ -175,14 +175,15 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
   
     __HAL_RCC_GPIOA_CLK_ENABLE();
     /**TIM1 GPIO Configuration    
+    PA7     ------> TIM1_CH1N
     PA8     ------> TIM1_CH1 
     */
-    GPIO_InitStruct.Pin = BuckPWM_Pin;
+    GPIO_InitStruct.Pin = BuckPWM_HI_Pin|BuckPWM_LO_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF2_TIM1;
-    HAL_GPIO_Init(BuckPWM_GPIO_Port, &GPIO_InitStruct);
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* USER CODE BEGIN TIM1_MspPostInit 1 */
 
